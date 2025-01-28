bad0e0929ab7f289ea5e35c15bd7ad5e9b352fcd
jordis.dev <jordislpm@gmail.com>
2025-01-27 20:30:13 -0500

    working at home page

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode }  from 'react'
 import styles from './layout.module.scss';
 import Header from './Header';
 import Footer from './Footer';
+ 
 
 interface LayoutProps {
     children: ReactNode;
@@ -9,9 +10,9 @@ interface LayoutProps {
 
 function Layout({ children }:LayoutProps) {
   return (
-    <div className={styles.content}>
+    <div className={styles.layout}>
         <Header/>
-        {children}
+      {children}
         <Footer/>
     </div>
   )
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from "./Home.module.scss"
+import background_image from "../../assets/images/bg.png"
 
 function Home() {
-  return (
-    <div className={styles.content}>Home</div>
-  )
+    return (
+        <div className={styles.home_page}>
+            <div className={styles.text_container}>
+                text
+            </div>
+            <div className={styles.img_container}>
+                <img src={background_image} alt="background_image" />
+            </div>
+        </div>
+    )
 }
 
 export default Home
